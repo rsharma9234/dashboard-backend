957f7d6dd05d973b39d129e5e583662ba35f3659
rohit sharma <rsharma9234@gmail.com>
2020-07-10 19:26:40 +0530

    fix issue

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -5,59 +5,70 @@ const accountModel = models.account;
 const mainLoginModel = models.main_login;
 
 const fetchAllAccounts = async (req, res, next) => {
-    try{
+    try {
         let accountInfo = await accountModel.findAll({
             attributes: { exclude: ['password'] },
             // where:{ status:1},
-            raw:true
+            raw: true
         });
-        return res.status(200).json({ rows: accountInfo});
+        return res.status(200).json({ rows: accountInfo });
 
-    } catch(err) {
+    } catch (err) {
         return res.status(err.status || 500).json(err);
     };
 }
 
 const addUser = async (req, res, next) => {
-    try{
-        let {login, password, broker, alias} = req.body;
+
+    try {
+        // console.log(req.body,"bodyHGGGHGHJGHJGH");
+
+        let { login, password, broker, alias } = req.body;
         let accountOneInfo = await accountModel.findOne({
-            where:{
-                login, password, broker
+            where: {
+                login: login
             }
         });
-        if(accountOneInfo){
-           await accountModel.update({status:1, alias}, { where:{ login }});
-           let accountInfo = await accountModel.findAll({
-               attributes: { exclude: ['password'] },
-               where:{ status:1}, raw:true
-           });
-           return res.status(200).json({ rows: accountInfo});
-        }else{
-            return res.status(200).json({ rows: 'error'});
+        console.log(accountOneInfo, "apiiiiiiiii");
+
+        console.log(accountOneInfo, '---test-----', accountModel, '--mod--')
+        if (accountOneInfo) {
+            //    await accountModel.update({status:1, alias}, { where:{ login }});
+            //    let accountInfo = await accountModel.findAll({
+            //        attributes: { exclude: ['password'] },
+            //        where:{ status:1}, raw:true
+            //    });
+
+            return res.json({ status: 202, rows: 'User already exist' });
+        } else {
+            console.log("---else---")
+            accountModel.create({ 'login': req.body.login, password: req.body.password, 'broker': broker, 'alias': alias, 'status': 0, active: 1 })
+            return res.status(200).json({ rows:'error' });
         }
-    
-    } catch(err) {
+
+    } catch (err) {
+        console.log(err,'errrrrrrrrrrr');
+        
         return res.status(err.status || 500).json(err);
     };
 }
 
 
 const mainLogin = async (req, res, next) => {
-    try{
-        let { username, password} = req.body;
-        let accountCheck= await mainLoginModel.findOne({
-            where:{ username},
-            raw:true
+    try {
+        let { username, password } = req.body;
+        let accountCheck = await mainLoginModel.findOne({
+            where: { username },
+            raw: true
         });
-        if(accountCheck===null){
+        if (accountCheck === null) {
             let err = new Error()
             err.status = 404;
             err.name = 'username';
             err.message = 'Invalid username.';
             return res.status(200).json(err);
         }
-        if(accountCheck.password !== password){
+        if (accountCheck.password !== password) {
             let err = new Error()
             err.status = 404;
             err.name = 'password';
@@ -66,32 +77,32 @@ const mainLogin = async (req, res, next) => {
         }
         let accountInfo = await mainLoginModel.findAll({
             attributes: { exclude: ['password'] },
-            where:{ username, password},
-            raw:true
+            where: { username, password },
+            raw: true
         });
-        return res.status(200).json({ rows: accountInfo});
-    } catch(err) {
+        return res.status(200).json({ rows: accountInfo });
+    } catch (err) {
         return res.status(err.status || 500).json(err);
     };
 }
 
 const updateUser = async (req, res, next) => {
-    try{
-      let {id,alias} = req.body;
-      let filterInfo = await accountModel.findOne({
-        where:{
-          id
-      }
-      });
-      if(filterInfo){
-          await accountModel.update({alias:alias}, { where:{ id }});
-          return res.status(200).json({ rows: "Updated"});
-     }
-    } catch(err) {
+    try {
+        let { id, alias } = req.body;
+        let filterInfo = await accountModel.findOne({
+            where: {
+                id
+            }
+        });
+        if (filterInfo) {
+            await accountModel.update({ alias: alias }, { where: { id } });
+            return res.status(200).json({ rows: "Updated" });
+        }
+    } catch (err) {
         return res.status(err.status || 500).json(err);
     };
-  }
+}
 
 module.exports = {
-    fetchAllAccounts, addUser, mainLogin,updateUser
+    fetchAllAccounts, addUser, mainLogin, updateUser
 };
